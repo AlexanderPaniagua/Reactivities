5dfcb4eb58ddff5d18e6cb4c0b93e5751dcaa19c
Alexander Paniagua <alexanderpaniiagua@gmail.com>
2019-12-13 10:57:34 -0600

    Dev3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API
                     opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
                 }
                 );
+            services.AddCors(opt => { opt.AddPolicy("CorsPolicy", policy => { policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"); }); });
             services.AddControllers();
         }
 
@@ -44,6 +45,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
